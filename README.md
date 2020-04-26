2d0886b5f45e805d5886df09f90692742ce0d01e
yaakov <yaakov.kuperman@sparkpost.com>
2020-04-26 19:49:14 -0400

    forgot the configure script, adding README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reviewbot!
+
+This bot will manage a list of code reviews for your team, allowing users to add/remove/list them.  This makes it easy for developers to register new reviews and then have reviewers come along later and find code to look at without anyone having to *gasp* talk to other people.
---
+++ configure_postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# this has to be scripted because initialization of the container errors
+# out if you place it beforehand
+cat > /var/lib/postgresql/data/postgresql.conf << CONFIG
+listen_addresses = '*'
+#log_min_messages = debug1
+#log_min_error_statement = debug1
+log_min_duration_statement = 0
+debug_pretty_print = on
+log_error_verbosity = verbose
+log_statement = 'all'
+#client_min_messages = debug1
+CONFIG
